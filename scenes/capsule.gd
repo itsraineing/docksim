9ff21cfbdd7bcbb519a8b16fb4d842c229d8fbfb
itsraineing <7527892+itsraineing@users.noreply.github.com>
2020-12-06 18:09:29 -0800

    implement status displays and precision controls

--- scenes/capsule.gd
+++ scenes/capsule.gd
@@ -7,40 +7,64 @@ enum capsules {
 
 var current_capsule: int = capsules.CAPSULE_DRAGON
 
+var precision_mode: bool = false
+
 var velocity: Vector2 = Vector2(0, 0)
 var rot_velocity: float = 0
-var thrust = [Vector3(15, 10, 0.5), Vector3(10, 15, 0.7)]
+var thrust = [Vector3(5, 5, 0.05), Vector3(5, 5, 0.07)]
+
+onready var dock_position = [$dragon/docking_center.position, $starliner/docking_center.position]
 
 func _ready() -> void:
 	set_physics_process(true)
-	$dragon.show()
-	$starliner.hide()
+	set_capsule(current_capsule)
+
+func get_dock_position() -> Vector2:
+	return dock_position[current_capsule]
+
+func set_capsule(capsule: int) -> void:
+	if capsule == capsules.CAPSULE_DRAGON:
+		current_capsule = capsules.CAPSULE_DRAGON
+		$dragon.show()
+		$starliner.hide()
+	else:
+		current_capsule = capsules.CAPSULE_STARLINER
+		$dragon.hide()
+		$starliner.show()
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("thrust_up"):
-		velocity -= Vector2(0, thrust[current_capsule].y).rotated(rotation)
-	if Input.is_action_pressed("thrust_down"):
-		velocity += Vector2(0, thrust[current_capsule].y).rotated(rotation)
-	if Input.is_action_pressed("thrust_backwards"):
-		velocity -= Vector2(thrust[current_capsule].x, 0).rotated(rotation)
-	if Input.is_action_pressed("thrust_forwards"):
-		velocity += Vector2(thrust[current_capsule].x, 0).rotated(rotation)
+	if precision_mode:
+		if Input.is_action_just_pressed("thrust_up"):
+			velocity -= Vector2(0, thrust[current_capsule].y).rotated(rotation)
+		if Input.is_action_just_pressed("thrust_down"):
+			velocity += Vector2(0, thrust[current_capsule].y).rotated(rotation)
+		if Input.is_action_just_pressed("thrust_backwards"):
+			velocity -= Vector2(thrust[current_capsule].x, 0).rotated(rotation)
+		if Input.is_action_just_pressed("thrust_forwards"):
+			velocity += Vector2(thrust[current_capsule].x, 0).rotated(rotation)
 
-	position += velocity * delta
+		if Input.is_action_just_pressed("thrust_ccw"):
+			rot_velocity -= thrust[current_capsule].z
+		if Input.is_action_just_pressed("thrust_cw"):
+			rot_velocity += thrust[current_capsule].z
+	else:
+		if Input.is_action_pressed("thrust_up"):
+			velocity -= Vector2(0, thrust[current_capsule].y).rotated(rotation)
+		if Input.is_action_pressed("thrust_down"):
+			velocity += Vector2(0, thrust[current_capsule].y).rotated(rotation)
+		if Input.is_action_pressed("thrust_backwards"):
+			velocity -= Vector2(thrust[current_capsule].x, 0).rotated(rotation)
+		if Input.is_action_pressed("thrust_forwards"):
+			velocity += Vector2(thrust[current_capsule].x, 0).rotated(rotation)
 
-	if Input.is_action_pressed("thrust_ccw"):
-		rot_velocity -= thrust[current_capsule].z
-	if Input.is_action_pressed("thrust_cw"):
-		rot_velocity += thrust[current_capsule].z
+		if Input.is_action_pressed("thrust_ccw"):
+			rot_velocity -= thrust[current_capsule].z
+		if Input.is_action_pressed("thrust_cw"):
+			rot_velocity += thrust[current_capsule].z
+
 
 	rotation += rot_velocity * delta
+	position += velocity * delta
 
 	if Input.is_action_just_pressed("ui_select"):
-		if current_capsule == capsules.CAPSULE_DRAGON:
-			current_capsule = capsules.CAPSULE_STARLINER
-			$dragon.hide()
-			$starliner.show()
-		else:
-			current_capsule = capsules.CAPSULE_DRAGON
-			$dragon.show()
-			$starliner.hide()
+		precision_mode = !precision_mode
--- scenes/stage.gd
+++ scenes/stage.gd
@@ -4,10 +4,13 @@ var window_size: Vector2 = Vector2(0, 0)
 var bg_size: Vector2 = Vector2(1920, 1440)
 var window_edge: Vector2 = Vector2(0, 0)
 var sim_edge_margin: Vector2 = Vector2(200, 265)
+var status_edge_margin: Vector2 = Vector2(20, 20)
 
 func _ready() -> void:
 	get_tree().get_root().connect("size_changed", self, "update_bg")
 	update_bg()
+
+	set_physics_process(true)
 	pass
 
 func update_bg() -> void:
@@ -29,3 +32,34 @@ func update_bg() -> void:
 
 	$sim.position.x = window_edge.x - sim_edge_margin.x
 	$sim.position.y = window_edge.y + sim_edge_margin.y
+
+	$status_display.position.x = -window_edge.x + status_edge_margin.x
+
+	$status_display/telemetry_label.position.y = window_edge.y + status_edge_margin.y
+	$status_display/precision_label.position.y = -window_edge.y - status_edge_margin.y
+
+func _physics_process(delta: float) -> void:
+	var capsule_dock: Vector2 = $sim/capsule.get_dock_position() + $sim/capsule.position
+	var capsule_rot = $sim/capsule.rotation
+	var ida_dock: Vector2 = $sim/ida.position
+
+	var rot_vector: Vector2 = Vector2(cos(capsule_rot), sin(capsule_rot))
+	var rot_distance: float = rot_vector.distance_to(Vector2(1, 0)) * 180
+	if sin(capsule_rot) < 0:
+		rot_distance = -rot_distance
+
+	var distance: Vector2 = capsule_dock - ida_dock
+	var label_string = "distance to port: {h}\nx: {x}\ny: {y}\nrotation: {r}"
+
+	var distance_str = str(distance.length()).pad_decimals(3)
+	var x_str = str(distance.x).pad_decimals(3)
+	var y_str = str(distance.y).pad_decimals(3)
+	var rot_str = str(rot_distance).pad_decimals(3)
+	label_string = label_string.format({"h" : distance_str, "x" : x_str, "y" : y_str, "r" : rot_str})
+
+	$status_display/telemetry_label/Label.text = label_string
+
+	if $sim/capsule.precision_mode:
+		$status_display/precision_label/Label.text = "Precision mode"
+	else:
+		$status_display/precision_label/Label.text = "Coarse mode"
